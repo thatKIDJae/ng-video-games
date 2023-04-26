417a7eb6e1d0999ff849bec8f950e4be447de5ee
Jason Pis-an (NCS) <jason.pisan@globe.com.ph>
2023-04-26 23:09:30 +0800

    Add more styles and svg files for the project.

--- src/app/models.ts
+++ src/app/models.ts
@@ -1,17 +1,17 @@
 export interface Game {
-    background_image: string;
-    name: string;
-    released: string;
-    metacritic_url: string;
-    website: string;
-    description: string;
-    metacritic: number;
-    genres: Array<Genre>;
-    parent_platforms: Array<ParentPlatform>;
-    publishers: Array<Publishers>;
-    ratings: Array<Rating>;
-    screenshots: Array<Screenshots>;
-    trailers: Array<Trailer>;
+  background_image: string;
+  name: string;
+  released: string;
+  metacritic_url: string;
+  website: string;
+  description: string;
+  metacritic: number;
+  genres: Array<Genre>;
+  parent_platforms: Array<ParentPlatform>;
+  publishers: Array<Publishers>;
+  ratings: Array<Rating>;
+  screenshots: Array<Screenshots>;
+  trailers: Array<Trailer>;
 }
 
 export interface APIResponse<T> {
@@ -24,7 +24,8 @@ interface Genre {
 
 interface ParentPlatform {
   platform: {
-    name: string;
+    name: string,
+    slug: string;
   };
 }
 
